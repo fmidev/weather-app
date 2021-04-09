dd8f77545bc5da9de9ab6ad669036e9f987109d2
Paavo Väänänen <paavo.vaananen@cgi.com>
2021-04-09 14:19:44 +0300

    initialize store for map

---
+++ src/store/map/selectors.ts
@@ -0,0 +1,25 @@
+import { Selector, createSelector } from 'reselect';
+import { State } from '../types';
+import { MapState, MapLayers } from './types';
+
+const selectMapDomain: Selector<State, MapState> = (state) => state.map;
+
+export const selectSliderTime = createSelector<State, MapState, number>(
+  selectMapDomain,
+  (map) => map.sliderTime
+);
+
+export const selectSliderStep = createSelector<State, MapState, number>(
+  selectMapDomain,
+  (map) => map.sliderStep
+);
+
+export const selectAnimateToArea = createSelector<State, MapState, boolean>(
+  selectMapDomain,
+  (map) => map.animateToArea
+);
+
+export const selectMapLayers = createSelector<State, MapState, MapLayers>(
+  selectMapDomain,
+  (map) => map.mapLayers
+);
