caa708f5efdb593c9a3918be853b59b741626233
Paavo Väänänen <paavo.vaananen@cgi.com>
2021-05-26 15:51:04 +0300

    fix coordinates

--- src/components/RainRadarOverlay.tsx
+++ src/components/RainRadarOverlay.tsx
@@ -26,20 +26,14 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 type RainRadarProps = PropsFromRedux;
 
 const BOUNDS = {
-  bottomLeft: [56.75131991843274, 10.215546158016812], // left bottom, min y, min x
-  bottomRight: [56.75131991843274, 37.37165878453536], // right bottom, min y, max x
-  topRight: [71.24172567164591, 37.37165878453536], // top right, max y, max x,
-  topLeft: [71.24172567164591, 10.215546158016812], // top left, max ly, min x
+  bottomLeft: [56.75132000097737, 16.86743001103862], // left bottom, min y, min x
+  bottomRight: [56.75132000097737, 37.37165999353797], // right bottom, min y, max x
+  topRight: [70.98305999744711, 37.37165999353797], // top right, max y, max x,
+  topLeft: [70.98305999744711, 16.86743001103862], // top left, max ly, min x
 } as { [key: string]: [number, number] };
 
-/* <BoundingBox CRS="CRS:84" minx="10.215546158016812" miny="56.75131991843274" maxx="37.37165878453536" maxy="71.24172567164591"/ */
-
-// TODO: should be visible without touching the slider
-// TODO: säädä kulmapisteet ja projektio EPSG:3857
-// 1877673.71982;7709459.58195
-// 4160194.16058;11396482.4557
 const OBSERVATION_URL = `http://wms.fmi.fi/fmi-apikey/${Config.API_KEY}/geoserver/Radar/wms?service=WMS&styles=&transparent=true&version=1.1.0&request=GetMap&layers=Radar%3Asuomi_rr_eureffin&bbox=1877673.71982%2C7709459.58195%2C4160194.16058%2C11396482.4557&width=485&height=768&srs=EPSG%3A3857&format=image%2Fpng`;
-const FORECAST_URL = `http://wms.fmi.fi/fmi-apikey/${Config.API_KEY}/geoserver/Radar/wms?service=WMS&styles=&transparent=true&version=1.1.0&request=GetMap&layers=Radar%3Asuomi_tuliset_rr_eureffin&bbox=-118331.366408356%2C6335621.16701424%2C875567.731906565%2C7907751.53726352&width=485&height=768&srs=EPSG%3A3067&format=image%2Fpng`;
+const FORECAST_URL = `http://wms.fmi.fi/fmi-apikey/${Config.API_KEY}/geoserver/Radar/wms?service=WMS&styles=&transparent=true&version=1.1.0&request=GetMap&layers=Radar%3Asuomi_tuliset_rr_eureffin&bbox=1877673.71982%2C7709459.58195%2C4160194.16058%2C11396482.4557&width=485&height=768&srs=EPSG%3A3857&format=image%2Fpng`;
 
 const RainRadarOverlay: React.FC<RainRadarProps> = ({ sliderTime }) => {
   const [hasPrefetched, setHasPrefetched] = useState<boolean>(false);
@@ -108,7 +102,7 @@ const RainRadarOverlay: React.FC<RainRadarProps> = ({ sliderTime }) => {
   const image = `${baseUrl}&time=${current}` as ImageURISource;
 
   if (!hasPrefetched) return null;
-  // console.log(image);
+  console.log(image);
   return <Overlay bounds={bounds} image={image} />;
 };
 
