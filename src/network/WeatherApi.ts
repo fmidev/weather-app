d4e2d12a149300356707e6882adeea38530d8d94
Mikko Kuoremaki <mikko.kuoremaki@fmi.fi>
2021-06-22 13:43:48 +0300

    rough outline of data fetch structure

---
+++ src/network/WeatherApi.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import Config from 'react-native-config';
+
+import { Location, WeatherData } from '../store/forecast/types';
+
+export const getForecast = async (location: Location): Promise<WeatherData> => {
+  const params = {
+    ...location,
+    starttime: 0,
+    endtime: 'data',
+    param: [
+      'geoid',
+      'epochtime',
+      'name',
+      'latitude',
+      'longitude',
+      'region',
+      'country',
+      'iso2',
+      'localtz',
+      'sunrise',
+      'sunset',
+      'sunrisetoday',
+      'sunsettoday',
+      'utctime',
+      'localtime',
+      'origintime',
+      'modtime',
+      'temperature',
+      'smartSymbol',
+      'pop',
+      'windspeedms',
+      'winddirection',
+      'windcompass8',
+      'precipitation1h',
+      'feelsLike',
+      'dark',
+    ].join(','),
+    format: 'json',
+    attributes: 'geoid',
+  };
+
+  const { data } = await axios.get(Config.WEEATHER_API_URL, {
+    params,
+  });
+  return data;
+};
+
+export const fetchObservations = async (geoid: Number) => {
+  const params = {
+    param: 'Temperature',
+    geoid,
+  };
+  const { data } = await axios.get(Config.WEEATHER_API_URL, {
+    params,
+  });
+  return data;
+};
