17a7abcb51647493203965356ccd0cd6ffcd7bc6
Paavo Väänänen <paavo.vaananen@cgi.com>
2021-11-11 16:44:51 +0200

    disable font scaling from Text and TextInput by default

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text, TextInput } from 'react-native';
 // eslint-disable-next-line import/extensions, import/no-unresolved
 import App from './App';
 import { name as appName } from './app.json';
+
+// disable font scaling for MVP
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
