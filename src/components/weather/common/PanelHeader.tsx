f45026fef2d6bff64a8a034d83246e255f52e5c9
Paavo Väänänen <paavo.vaananen@cgi.com>
2022-02-02 10:13:40 +0200

    add lastUpdated to PanelHeader

--- src/components/weather/common/PanelHeader.tsx
+++ src/components/weather/common/PanelHeader.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
 
-import { WHITE, CustomTheme } from '@utils/colors';
+import { CustomTheme } from '@utils/colors';
 
 type PanelHeaderProps = {
   title: string;
+  lastUpdated?: string | false;
 };
 
-const PanelHeader: React.FC<PanelHeaderProps> = ({ title }) => {
+const PanelHeader: React.FC<PanelHeaderProps> = ({ title, lastUpdated }) => {
   const { colors } = useTheme() as CustomTheme;
+  const { t } = useTranslation('forecast');
   return (
-    <View style={[styles.cardHeader, { backgroundColor: colors.cardHeader }]}>
-      <Text style={[styles.headerTitle, { color: WHITE }]}>{title}</Text>
+    <View
+      style={[
+        styles.cardHeader,
+        {
+          backgroundColor: colors.background,
+          borderBottomColor: colors.border,
+        },
+      ]}>
+      <Text style={[styles.headerTitle, { color: colors.primaryText }]}>
+        {title}
+      </Text>
+      {lastUpdated && (
+        <Text style={[styles.updatedText, { color: colors.hourListText }]}>
+          {t('updated')} <Text style={styles.bold}>{lastUpdated}</Text>
+        </Text>
+      )}
     </View>
   );
 };
@@ -22,11 +39,21 @@ const styles = StyleSheet.create({
     height: 44,
     borderTopRightRadius: 8,
     borderTopLeftRadius: 8,
-    paddingVertical: 12,
-    paddingLeft: 16,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   headerTitle: {
-    fontSize: 16,
+    fontSize: 18,
+    fontFamily: 'Roboto-Bold',
+  },
+  updatedText: {
+    fontSize: 14,
+    fontFamily: 'Roboto-Regular',
+  },
+  bold: {
     fontFamily: 'Roboto-Bold',
   },
 });
