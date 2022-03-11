2d52914d3d828323bc914038898c442443b64647
Paavo Väänänen <paavo.vaananen@cgi.com>
2022-03-11 11:46:04 +0200

    include callout info on selected marker

--- src/components/map/layers/TimeseriesMarker.tsx
+++ src/components/map/layers/TimeseriesMarker.tsx
@@ -1,12 +1,13 @@
-import React, { MutableRefObject, useRef } from 'react';
+import React, { MutableRefObject, useRef, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Marker, LatLng } from 'react-native-maps';
 import { useTheme } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
 
 import { weatherSymbolGetter } from '@assets/images';
 import { CustomTheme } from '@utils/colors';
 
-import TimeseriesCallout from './TimeseriesCallout';
+import Icon from '@components/common/Icon';
 
 type TimeseriesMarkerProps = {
   name: string;
@@ -25,18 +26,19 @@ const TimeseriesMarker: React.FC<TimeseriesMarkerProps> = ({
   windDirection,
   windSpeedMS,
 }) => {
+  const { t } = useTranslation();
+  const { colors, dark } = useTheme() as CustomTheme;
   const markerRef = useRef() as MutableRefObject<Marker>;
-  const { colors } = useTheme() as CustomTheme;
+  const [isSelected, setIsSelected] = useState<boolean>(false);
 
   return (
     <Marker
       ref={markerRef}
       coordinate={coordinate}
       tracksViewChanges={false}
-      onCalloutPress={() => {
-        markerRef.current.hideCallout();
-      }}
-      pointerEvents="auto">
+      pointerEvents="auto"
+      onSelect={() => setIsSelected(true)}
+      onDeselect={() => setIsSelected(false)}>
       <View
         style={[
           styles.markerContainer,
@@ -45,39 +47,82 @@ const TimeseriesMarker: React.FC<TimeseriesMarkerProps> = ({
             borderColor: colors.mapButtonBorder,
           },
         ]}>
-        {weatherSymbolGetter(
-          smartSymbol.toString(),
-          false
-        )?.({
-          width: 40,
-          height: 40,
-        })}
-        <Text
-          style={[
-            styles.tempText,
-            { color: colors.text },
-          ]}>{`${temperature}°`}</Text>
+        <View style={styles.mainRow}>
+          {weatherSymbolGetter(
+            smartSymbol.toString(),
+            false
+          )?.({
+            width: 40,
+            height: 40,
+          })}
+          <Text
+            style={[
+              styles.tempText,
+              { color: colors.text },
+            ]}>{`${temperature}°`}</Text>
+        </View>
+        {isSelected && (
+          <View style={styles.calloutContainer}>
+            <Text style={[styles.calloutTitle, { color: colors.text }]}>
+              {name}
+            </Text>
+            <Text style={[styles.calloutText, { color: colors.text }]}>
+              {`${t(`symbols:${smartSymbol}`)}`}
+            </Text>
+            <View style={styles.windSpeedRow}>
+              <Icon
+                name={dark ? 'wind-dark' : 'wind-light-map'}
+                width={24}
+                height={24}
+                style={{
+                  transform: [
+                    {
+                      rotate: `${(windDirection || 0) + 45 - 180}deg`,
+                    },
+                  ],
+                }}
+              />
+              <Text style={[styles.calloutText, { color: colors.text }]}>
+                {`${windSpeedMS} m/s`}
+              </Text>
+            </View>
+          </View>
+        )}
       </View>
-      <TimeseriesCallout
-        item={{ name, smartSymbol, windDirection, windSpeedMS }}
-      />
     </Marker>
   );
 };
 
 const styles = StyleSheet.create({
   markerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
     paddingHorizontal: 4,
     borderWidth: 1,
     borderRadius: 4,
-    maxWidth: 75,
+  },
+  mainRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   tempText: {
     fontSize: 14,
     fontFamily: 'Roboto-Medium',
     marginLeft: 4,
+  },
+  calloutContainer: {
+    paddingHorizontal: 4,
+    paddingBottom: 4,
+  },
+  calloutTitle: {
+    fontFamily: 'Roboto-Medium',
+    fontSize: 16,
+  },
+  calloutText: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
+  },
+  windSpeedRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
