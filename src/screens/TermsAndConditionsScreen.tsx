6db439987c14814beaab40aed5241fdcb91a13d5
Paavo Väänänen <paavo.vaananen@cgi.com>
2022-03-17 16:15:53 +0200

    add close button to setup terms view

--- src/screens/TermsAndConditionsScreen.tsx
+++ src/screens/TermsAndConditionsScreen.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Text, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@react-navigation/native';
 
-import { CustomTheme } from '@utils/colors';
+import { CustomTheme, WHITE } from '@utils/colors';
 
-const TermsAndConditionsScreen = () => {
+type TermsAndConditionsScreenProps = {
+  showCloseButton?: boolean;
+  onClose?: () => void;
+};
+
+const TermsAndConditionsScreen: React.FC<TermsAndConditionsScreenProps> = ({
+  showCloseButton,
+  onClose,
+}) => {
   const { t } = useTranslation('termsAndConditions');
   const { colors } = useTheme() as CustomTheme;
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollContainer}>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors.background }]}>
+      <ScrollView
+        style={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}>
         <Text style={[styles.title, { color: colors.primaryText }]}>
           {t('generalTitle')}
         </Text>
@@ -30,6 +48,18 @@ const TermsAndConditionsScreen = () => {
           {t('loremIpsum')}
         </Text>
       </ScrollView>
+      {showCloseButton && (
+        <View
+          style={[styles.closeButtonContainer, { shadowColor: colors.shadow }]}>
+          <TouchableOpacity onPress={onClose}>
+            <View style={[styles.closeButton, { borderColor: colors.text }]}>
+              <Text style={[styles.closeText, { color: colors.text }]}>
+                {t('close')}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
@@ -51,6 +81,31 @@ const styles = StyleSheet.create({
   body: {
     fontSize: 16,
     fontFamily: 'Roboto-Regular',
+  },
+  closeButtonContainer: {
+    backgroundColor: WHITE,
+    width: '100%',
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 1,
+    elevation: 11,
+    alignItems: 'center',
+    paddingVertical: 16,
+  },
+  closeButton: {
+    width: 120,
+    height: 44,
+    borderRadius: 24,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeText: {
+    fontSize: 16,
+    fontFamily: 'Roboto-Medium',
   },
 });
 
