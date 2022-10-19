34814e5889be72a4e858df284198725ae0a705f6
Paavo Väänänen <paavo.vaananen@cgi.com>
2022-10-19 12:54:29 +0100

    show info to only active parameters

--- src/store/forecast/constants.ts
+++ src/store/forecast/constants.ts
@@ -4,8 +4,9 @@ export const FEELS_LIKE = 'feelsLike';
 export const WIND_SPEED_AND_DIRECTION = 'windSpeedMSwindDirection';
 export const WIND_GUST = 'hourlymaximumgust';
 export const PRECIPITATION_1H = 'precipitation1h';
-// export const SNOW_FALL = 'snowfall'; // TODO: check
+export const SNOW_FALL = 'snowfall'; // TODO: check
 export const PRECIPITATION_PROBABILITY = 'pop';
+export const THUNDER_PROBABILITY = 'probabilityThunderstorm';
 export const DEW_POINT = 'dewPoint';
 export const RELATIVE_HUMIDITY = 'relativeHumidity';
 export const PRESSURE = 'pressure';
@@ -31,11 +32,12 @@ export default [
   WIND_SPEED_AND_DIRECTION,
   WIND_GUST,
   PRECIPITATION_1H,
-  // SNOW_FALL,
   PRECIPITATION_PROBABILITY,
   DEW_POINT,
   RELATIVE_HUMIDITY,
   PRESSURE,
   UV_CUMULATED,
   DAY_LENGTH,
+  SNOW_FALL,
+  THUNDER_PROBABILITY,
 ];
