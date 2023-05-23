e86ce0eb7fb79759cd08b075ce012e42a30ba475
Antti Rumpunen <antti.rumpunen@fmi.fi>
2023-05-23 16:34:48 +0300

    Added option to use cardinals on wind direction in next hour forecast panel

--- src/components/weather/NextHourForecastPanel.tsx
+++ src/components/weather/NextHourForecastPanel.tsx
@@ -116,6 +116,36 @@ const NextHourForecastPanel: React.FC<NextHourForecastPanelProps> = ({
     nextHourForecast.precipitation1h
   );
 
+  // wind direction can be with either one degree accuracy or uses cardinals (N,NE,E,...)
+  const useCardinals =
+    Config.get('weather').forecast.useCardinalsForWindDirection;
+  let direction = 0;
+  if (useCardinals) {
+    const tempDir = nextHourForecast.windDirection || 0;
+    if ((tempDir >= 338 && tempDir <= 360) || (tempDir >= 0 && tempDir <= 22)) {
+      direction = 0; // N
+    } else if (tempDir >= 23 && tempDir <= 67) {
+      direction = 45; // NE
+    } else if (tempDir >= 68 && tempDir <= 112) {
+      direction = 90; // E
+    } else if (tempDir >= 113 && tempDir <= 157) {
+      direction = 135; // SE
+    } else if (tempDir >= 158 && tempDir <= 202) {
+      direction = 180; // S
+    } else if (tempDir >= 203 && tempDir <= 247) {
+      direction = 225; // SW
+    } else if (tempDir >= 248 && tempDir <= 292) {
+      direction = 270; // W
+    } else if (tempDir >= 293 && tempDir <= 337) {
+      direction = 315; // NW
+    }
+    // for some reason icon is pointing NW instead of N => +45
+    // wind value needs 180 degree switch to show correctly where wind is coming from
+    direction = direction + 45 - 180;
+  } else {
+    direction = (nextHourForecast.windDirection || 0) + 45 - 180;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.alignCenter} accessible accessibilityRole="header">
@@ -220,9 +250,7 @@ const NextHourForecastPanel: React.FC<NextHourForecastPanelProps> = ({
                 {
                   transform: [
                     {
-                      rotate: `${
-                        (nextHourForecast.windDirection || 0) + 45 - 180
-                      }deg`,
+                      rotate: `${direction}deg`,
                     },
                   ],
                 },
