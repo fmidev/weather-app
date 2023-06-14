e005e61082caec4a5e4e81a88c7ceeeb1b6e3e9d
Antti Rumpunen <antti.rumpunen@fmi.fi>
2023-06-14 13:38:47 +0300

    issue-488 removed unnecessary if

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -231,11 +231,9 @@ const getWMSLayerUrlsAndBounds = async (
         filteredLayers = Layer.filter((L: WmsLayer) =>
           allLayerNames.includes(L.Name)
         );
-      } else if (Object.prototype.toString.call(Layer) === '[object Object]') {
+      } else if (allLayerNames.includes(Layer.Name)) {
         // layer is the only layer returned
-        if (allLayerNames.includes(Layer.Name)) {
-          filteredLayers.push(Layer);
-        }
+        filteredLayers.push(Layer);
       }
 
       capabilitiesData.set(src, filteredLayers);
