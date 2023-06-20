dc4f1651c57e8dd5ee945b9a82c1a1ceb017af90
Valtteri Nurminen <valtteri.nurminen@cgi.com>
2023-06-20 11:58:25 +0300

    Small fixes and adjustments to DailyChart

--- src/components/weather/charts/DailyChart.tsx
+++ src/components/weather/charts/DailyChart.tsx
@@ -15,15 +15,22 @@ const DailyChart: React.FC<ChartDataProps> = ({
     minimumGroundTemperature06,
   } = chartValues;
 
+  const rrdayData = rrday.filter((dataItem) => dataItem?.y && dataItem.y >= 0);
+  const extremeTemperaturesData = maximumTemperature.map((maxT, index) => ({
+    x: maxT?.x ?? null,
+    y: maxT?.y ?? null,
+    y0: minimumTemperature[index]?.y ?? null,
+  }));
+
   return (
     <VictoryGroup theme={chartTheme} width={chartWidth}>
-      {rrday && rrday.length > 0 && (
+      {rrdayData && rrdayData.length > 0 && (
         <VictoryBar
           data={rrday.filter((dataItem) => dataItem?.y && dataItem.y >= 0)}
           domain={chartDomain}
           key="secondary"
-          alignment="middle"
-          barWidth={8}
+          alignment="start"
+          barWidth={10}
           style={{
             data: {
               fill: 'rgb(30, 110, 214)',
@@ -47,13 +54,9 @@ const DailyChart: React.FC<ChartDataProps> = ({
       {maximumTemperature?.length > 0 && minimumTemperature?.length > 0 && (
         <VictoryBar
           alignment="middle"
-          barWidth={8}
+          barWidth={10}
           domain={chartDomain}
-          data={maximumTemperature.map((maxT, index) => ({
-            x: maxT?.x ?? null,
-            y: maxT?.y ?? null,
-            y0: minimumTemperature[index]?.y ?? null,
-          }))}
+          data={extremeTemperaturesData}
           style={{
             data: {
               fill: 'rgb(145, 0, 0)',
