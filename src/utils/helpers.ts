4612b4e5521d5c6796b2fe17564b537d5ade2dab
Pekka Keränen <pekka.keranen@geosaaga.fi>
2024-04-16 14:24:23 +0300

    issue-554: fix incorrect comparison of observation values

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -220,7 +220,7 @@ export const getObservationCellValue = (
           decimals
         )
       : dividedValue;
-    if (!value) return '-';
+    if (value === null) return '-';
 
     return `${(unitParameterObject
       ? value
