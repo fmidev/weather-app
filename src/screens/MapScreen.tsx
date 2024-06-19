a51e8a863e2e7d4a33baf5638d9e40ff43558d6a
Pekka Keränen <pekka.keranen@geosaaga.fi>
2024-06-19 11:06:20 +0300

    TypeScript fix for react-native-maps 0.31.1

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { View, StyleSheet, Platform } from 'react-native';
 import MapView, { Camera, Region } from 'react-native-maps';
-import type { MapPressEvent } from 'react-native-maps';
+import type { MapEvent } from 'react-native-maps';
 import RBSheet from 'react-native-raw-bottom-sheet';
 import { useTheme, useIsFocused } from '@react-navigation/native';
 import { getDistance } from 'geolib';
@@ -165,7 +165,7 @@ const MapScreen: React.FC<MapScreenProps> = ({
       }
     }
   };
-  const onPress = (e: MapPressEvent) => {
+  const onPress = (e: MapEvent<{}>) => {
     if (e.nativeEvent.action !== 'marker-press') {
       updateSelectedCallout(undefined);
     }
