31cc51bc843187c403329ec8a1d6919dd511690f
Pekka Keränen <pekka.keranen@geosaaga.fi>
2025-04-17 12:22:10 +0300

    issue-828: Fixes to import paths and removed one unused file

--- src/components/common/CloseButton.tsx
+++ src/components/common/CloseButton.tsx
@@ -5,7 +5,7 @@ import { useTheme } from '@react-navigation/native';
 import { CustomTheme } from '@assets/colors';
 import AccessibleTouchableOpacity from './AccessibleTouchableOpacity';
 
-import Icon from '../../assets/Icon';
+import Icon from '@assets/Icon';
 
 type CloseButtonProps = {
   onPress: () => void;
