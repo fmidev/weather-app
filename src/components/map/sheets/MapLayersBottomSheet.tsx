768c943878779d625413312351c2de23c3eaa040
Pekka Keränen <pekka.keranen@geosaaga.fi>
2025-10-01 14:30:12 +0300

    issue-951: Adjusted Switch size for iOS 26

--- src/components/map/sheets/MapLayersBottomSheet.tsx
+++ src/components/map/sheets/MapLayersBottomSheet.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
-import { View, StyleSheet, Text, Switch } from 'react-native';
+import { View, StyleSheet, Text, Switch, Platform } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@react-navigation/native';
 
@@ -98,9 +98,10 @@ const MapLayersBottomSheet: React.FC<MapLayersBottomSheetProps> = ({
               {t('map:layersBottomSheet:locationHint')}
             </Text>
             <Switch
+              style={Platform.OS === 'ios' && Number(Platform.Version) >= 26 ? styles.wideSwitch : null }
               trackColor={{ false: GRAYISH_BLUE, true: SECONDARY_BLUE }}
               thumbColor={WHITE}
-              ios_backgroundColor={GRAYISH_BLUE}
+              ios_backgroundColor={WHITE}
               value={mapLayers.location}
               onValueChange={() => {
                 trackMatomoEvent('User action', 'Map', 'Show own location - '+!mapLayers.location);
@@ -206,6 +207,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: 'Roboto-Regular',
     flexShrink: 1,
+  },
+  wideSwitch: {
+    width: 65, // wider switch for iOS 26
   },
 });
 
