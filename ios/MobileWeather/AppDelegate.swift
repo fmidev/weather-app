91cd6937f03d5e47df6866fcbb05acbf536286a8
Pekka Keränen <pekka.keranen@geosaaga.fi>
2025-11-26 18:25:42 +0200

    issue-1104: Clear iOS cache on every startup

--- ios/MobileWeather/AppDelegate.swift
+++ ios/MobileWeather/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    
+    // Clear cache because of old forecast data issues
+    URLCache.shared.removeAllCachedResponses()
+    
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
