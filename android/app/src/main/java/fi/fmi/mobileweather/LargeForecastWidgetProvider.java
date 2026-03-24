2bd83bbcfb792ce86615a1d3be4a47aa69b3e7d4
Pekka Keränen <pekka.keranen@geosaaga.fi>
2026-03-24 08:44:20 +0200

    Removed WidgetProvider classes

--- android/app/src/main/java/fi/fmi/mobileweather/LargeForecastWidgetProvider.java
+++
@@ -1,4 +0,0 @@
-package fi.fmi.mobileweather;
-
-public class LargeForecastWidgetProvider extends fi.fmi.mobileweather.widgets.LargeForecastWidgetProvider {
-}
--- android/app/src/main/java/fi/fmi/mobileweather/SmallForecastWidgetProvider.java
+++
@@ -1,5 +0,0 @@
-package fi.fmi.mobileweather;
-
-public class SmallForecastWidgetProvider extends fi.fmi.mobileweather.widgets.SmallForecastWidgetProvider {
-}
-
