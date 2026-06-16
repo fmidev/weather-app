1578b5162227a9e49242dba505b56a2bae60fcbb
Pekka Keränen <pekka.keranen@geosaaga.fi>
2026-06-16 12:46:56 +0300

    issue-1371: Added datetime-settings to testConfig.ts

--- __tests__/config/testConfig.ts
+++ __tests__/config/testConfig.ts
@@ -112,6 +112,29 @@ const defaultConfig: ConfigType = {
       light: true,
       dark: true,
     },
+    dateTime: {
+      default: {
+        date: 'D.M.',
+        dateYear: 'D.M.YYYY',
+        longDate: 'Udddd, D.M.',
+        time: 'HH:mm',
+        dateTime: 'D.M. HH:mm',
+        longDateTime: 'D.M.YYYY HH:mm',
+        weekdayAbbreviation: 'Udd',
+        weekday: 'Udddd',
+        weekdayAndDate: 'dddd D.M.',
+        weekdayAbbreviationAndDate: 'Udd D.M.',
+      },
+      locales: {
+        en: {
+          date: 'D MMM',
+          dateTime: 'D MMM HH:mm',
+          weekdayAbbreviation: 'Uddd',
+          weekdayAndDate: 'Udddd D MMM',
+          weekdayAbbreviationAndDate: 'Uddd D MMM',
+        },
+      }
+    },
   },
   announcements: {
     enabled: false,
